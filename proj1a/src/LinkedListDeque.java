2e172c5775d1cdd22a04d47305dbec2b86140347
Haitao Huang <83610725+v0rt3xh@users.noreply.github.com>
2021-06-01 11:42:18 -0400

    Update LinkedListDeque.java

--- proj1a/src/LinkedListDeque.java
+++ proj1a/src/LinkedListDeque.java
@@ -72,7 +72,7 @@ public class LinkedListDeque<YourType> {
     public void printDeque() {
         GNode cursor = sentinel;
         System.out.println("======Printing process Start======");
-        while (cursor.next != sentinel) {
+        while (cursor.next != sentinel) { 
             cursor = cursor.next;
             System.out.print(cursor.item);
             System.out.print(" ");
@@ -82,7 +82,7 @@ public class LinkedListDeque<YourType> {
     /**Could be cumbersome when using one sentinel (not circular)*/
     /**I think it would be better removing unnecessary references*/
     public YourType removeFirst() {
-        if (size == 0) {
+        if (size == 0) { 
             return null;
         }
         YourType rValue = sentinel.next.item;
