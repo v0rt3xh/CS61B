b412be5e7c3d45d144a22e4ca15e6d5c52ef006e
Haitao Huang <83610725+v0rt3xh@users.noreply.github.com>
2021-06-01 11:30:00 -0400

    Update ArrayDeque.java

--- proj1a/src/ArrayDeque.java
+++ proj1a/src/ArrayDeque.java
@@ -8,28 +8,28 @@ public class ArrayDeque<YourType> {
     int last;
 
     /**Make it easier to find "before"*/
-    public int minusOne(int index){
-        if(index > 0){
+    public int minusOne(int index) {
+        if (index > 0) {
             return index - 1;
         }
         return items.length - 1;
     }
     /**Make it easier to move "pointer"*/
-    public int plusOne(int index){
+    public int plusOne(int index) {
         return (index + 1) % items.length;
     }
     /**Resize function*/
-    public void resize(){
-        if(size == items.length) expand();
+    public void resize() {
+        if (size == items.length) expand();
         float usage = (float) size / (float) items.length;
-        if(usage < 0.25 && items.length > capacity) shrink();
+        if (usage < 0.25 && items.length > capacity) shrink();
     }
     /**shrink current storage array*/
-    public void shrink(){//shrink by half
+    public void shrink() {//shrink by half
         int nextCapacity = items.length / 2;
-        YourType[] tmp=(YourType[]) new Object[nextCapacity];
+        YourType[] tmp = (YourType[]) new Object[nextCapacity];
         int start = (nextCapacity - size) / 2;
-        for(int i = start;i < (start + size);i++){
+        for (int i = start; i < (start + size); i++) {
             tmp[i] = this.get(i % start);
         }
         items = tmp;
@@ -37,11 +37,11 @@ public class ArrayDeque<YourType> {
         first = minusOne(start);
     }
     /**Expand current storage array*/
-    public void expand(){
+    public void expand() {
         int nextCapacity = items.length * 2;
         YourType[] tmp = (YourType[]) new Object[nextCapacity];
         int start = (nextCapacity - size) / 2;
-        for(int i = start;i < (start + size);i++){
+        for (int i = start; i < (start + size); i++) {
             tmp[i] = this.get(i % start);
         }
         items = tmp;
@@ -50,13 +50,13 @@ public class ArrayDeque<YourType> {
     }
 
     /**Add a sentinel node*/
-    public ArrayDeque(){
+    public ArrayDeque() {
         size = 0;
         items = (YourType[]) new Object[capacity];
         first = 3;
         last = 3;
     }
-    public ArrayDeque(YourType x){
+    public ArrayDeque(YourType x) {
         first = 3;
         last = 3;
         items = (YourType[]) new Object[capacity];
@@ -66,7 +66,7 @@ public class ArrayDeque<YourType> {
         size = 1;
     }
     /**Add an element to the front.*/
-    public void addFirst(YourType input){
+    public void addFirst(YourType input) {
         resize();
         items[first] = input;
         first = minusOne(first);
@@ -82,22 +82,22 @@ public class ArrayDeque<YourType> {
         size++;
     }
     /**Check if it's empty*/
-    public boolean isEmpty(){
-        if(size == 0){
+    public boolean isEmpty() {
+        if (size == 0) {
             return true;
         }
         return false;
     }
     /**Return the size of our array*/
-    public int size(){
+    public int size() {
         System.out.println(items.length);
         return size;
     }
     /**Print the ArrayDequeList*/
-    public void printDeque(){
+    public void printDeque() {
         System.out.println("======Printing process Start======");
         int counter = plusOne(first);
-        while(counter != minusOne(last)){
+        while (counter != minusOne(last)) {
             System.out.print(items[counter]);
             System.out.print(" ");
             counter = plusOne(counter);
@@ -106,8 +106,8 @@ public class ArrayDeque<YourType> {
         System.out.println("\n======Printing process End======");
     }
     /**Get the index-th element*/
-    public YourType get(int index){
-        if(index >= size || index < 0){
+    public YourType get(int index) {
+        if (index >= size || index < 0) {
             System.out.println("Error: The given index is out of bound");
             return (YourType) null;
         }
@@ -115,24 +115,24 @@ public class ArrayDeque<YourType> {
         return items[outIndex];
     }
     /**Remove the first element*/
-    public YourType removeFirst(){
-        if(size == 0) return (YourType) null;
+    public YourType removeFirst() {
+        if (size == 0) return (YourType) null;
         YourType outPut;
         first = plusOne(first);
         outPut = items[first];
         size--;
-        if(size == 0) last = minusOne(last);
+        if (size == 0) last = minusOne(last);
         resize();
         return outPut;
     }
     /**Remove the last element*/
-    public YourType removeLast(){
-        if(size==0) return (YourType) null;
+    public YourType removeLast() {
+        if (size==0) return (YourType) null;
         YourType outPut;
         last = minusOne(last);
         outPut = items[last];
         size--;
-        if(size == 0) first = plusOne(first);
+        if (size == 0) first = plusOne(first);
         resize();
         return outPut;
     }
