8d7b5f27c6190438401a68615922ee910f5a22e9
Haitao Huang <83610725+v0rt3xh@users.noreply.github.com>
2021-06-01 11:21:22 -0400

    Update ArrayDeque.java

--- proj1a/src/ArrayDeque.java
+++ proj1a/src/ArrayDeque.java
@@ -1,7 +1,7 @@
 import java.io.ObjectStreamException;
 
 public class ArrayDeque<YourType> {
-    private final int capacity=8;
+    private final int capacity = 8;
     YourType[] items;
     int size;
     int first;
@@ -9,81 +9,81 @@ public class ArrayDeque<YourType> {
 
     /**Make it easier to find "before"*/
     public int minusOne(int index){
-        if(index>0){
-            return index-1;
+        if(index > 0){
+            return index - 1;
         }
-        return items.length-1;
+        return items.length - 1;
     }
     /**Make it easier to move "pointer"*/
     public int plusOne(int index){
-        return (index+1)%items.length;
+        return (index + 1) % items.length;
     }
     /**Resize function*/
     public void resize(){
-        if(size==items.length) expand();
-        float usage=(float) size/ (float) items.length;
-        if(usage < 0.25 && items.length>capacity) shrink();
+        if(size == items.length) expand();
+        float usage = (float) size / (float) items.length;
+        if(usage < 0.25 && items.length > capacity) shrink();
     }
     /**shrink current storage array*/
     public void shrink(){//shrink by half
-        int nextCapacity=items.length/2;
+        int nextCapacity = items.length / 2;
         YourType[] tmp=(YourType[]) new Object[nextCapacity];
-        int start=(nextCapacity-size)/2;
-        for(int i=start;i<(start+size);i++){
-            tmp[i]=this.get(i%start);
+        int start = (nextCapacity - size) / 2;
+        for(int i = start;i < (start + size);i++){
+            tmp[i] = this.get(i % start);
         }
-        items=tmp;
-        last=plusOne(start+size-1);
-        first=minusOne(start);
+        items = tmp;
+        last = plusOne(start + size - 1);
+        first = minusOne(start);
     }
     /**Expand current storage array*/
     public void expand(){
-        int nextCapacity=items.length*2;
-        YourType[] tmp=(YourType[]) new Object[nextCapacity];
-        int start=(nextCapacity-size)/2;
-        for(int i=start;i<(start+size);i++){
-            tmp[i]=this.get(i%start);
+        int nextCapacity = items.length * 2;
+        YourType[] tmp = (YourType[]) new Object[nextCapacity];
+        int start = (nextCapacity - size) / 2;
+        for(int i = start;i < (start + size);i++){
+            tmp[i] = this.get(i % start);
         }
-        items=tmp;
-        last=plusOne(start+size-1);
-        first=minusOne(start);
+        items = tmp;
+        last = plusOne(start + size - 1);
+        first = minusOne(start);
     }
 
     /**Add a sentinel node*/
     public ArrayDeque(){
-        size=0;
-        items=(YourType[]) new Object[capacity];
-        first=3;
-        last=3;
+        size = 0;
+        items = (YourType[]) new Object[capacity];
+        first = 3;
+        last = 3;
     }
     public ArrayDeque(YourType x){
-        first=3;
-        last=3;
-        items=(YourType[]) new Object[capacity];
-        items[first]=x;
-        first=minusOne(first);
-        last=plusOne(last);
-        size=1;
+        first = 3;
+        last = 3;
+        items = (YourType[]) new Object[capacity];
+        items[first] = x;
+        first = minusOne(first);
+        last = plusOne(last);
+        size = 1;
     }
     /**Add an element to the front.*/
     public void addFirst(YourType input){
         resize();
-        items[first]=input;
-        first=minusOne(first);
-        if(size==0) last=plusOne(last);
+        items[first] = input;
+        first = minusOne(first);
+        if(size == 0) last = plusOne(last);
         size++;
     }
     /**Add an element to the end*/
     public void addLast(YourType input){
         resize();
-        items[last]=input;
-        last=plusOne(last);
-        if(size==0) first=minusOne(first);
+        items[last] = input;
+        last = plusOne(last);
+        if(size == 0) first = minusOne(first);
         size++;
     }
     /**Check if it's empty*/
     public boolean isEmpty(){
-        if(size==0){
+        if(size == 0){
             return true;
         }
         return false;
@@ -96,32 +96,32 @@ public class ArrayDeque<YourType> {
     /**Print the ArrayDequeList*/
     public void printDeque(){
         System.out.println("======Printing process Start======");
-        int counter=plusOne(first);
-        while(counter!=minusOne(last)){
+        int counter = plusOne(first);
+        while(counter != minusOne(last)){
             System.out.print(items[counter]);
             System.out.print(" ");
-            counter=plusOne(counter);
+            counter = plusOne(counter);
         }
         System.out.print(items[counter]);
         System.out.println("\n======Printing process End======");
     }
     /**Get the index-th element*/
     public YourType get(int index){
-        if(index >=size||index<0){
+        if(index >= size || index < 0){
             System.out.println("Error: The given index is out of bound");
             return (YourType) null;
         }
-        int outIndex=(index+plusOne(first))% items.length;
+        int outIndex = (index + plusOne(first)) % items.length;
         return items[outIndex];
     }
     /**Remove the first element*/
     public YourType removeFirst(){
-        if(size==0) return (YourType) null;
+        if(size == 0) return (YourType) null;
         YourType outPut;
-        first=plusOne(first);
-        outPut=items[first];
+        first = plusOne(first);
+        outPut = items[first];
         size--;
-        if(size==0) last=minusOne(last);
+        if(size == 0) last = minusOne(last);
         resize();
         return outPut;
     }
@@ -129,14 +129,11 @@ public class ArrayDeque<YourType> {
     public YourType removeLast(){
         if(size==0) return (YourType) null;
         YourType outPut;
-        last=minusOne(last);
-        outPut=items[last];
+        last = minusOne(last);
+        outPut = items[last];
         size--;
-        if(size==0) first=plusOne(first);
+        if(size == 0) first = plusOne(first);
         resize();
         return outPut;
     }
-
-
-
 }
